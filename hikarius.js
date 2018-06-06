ca42609c2233eef90ffe4f94a34f343ee117f3cd
Glazko <39458709+Glazko@users.noreply.github.com>
2018-06-06 07:34:03 +0200

    Update hikarius.js

--- hikarius.js
+++ hikarius.js
@@ -43,7 +43,7 @@ client.on('message', message => {
         message.author.send(help_embed);
         message.delete()        
 
-        return message.channel.send("<:logohikariuspng:450625849662767104> | Le problème de la commande sera réglé demain. ")
+        return message.channel.send("<:logohikariuspng:450625849662767104> | Le problème de la commande sera réglé tout à l'heure. ")
 }
 
 //if(message.content === prefix + "help"){
